45f21bc0da700c52bb9dab6efb762c4a2b5ff056
malk lasaad <116892032+malekbenyousef@users.noreply.github.com>
2023-12-17 13:46:22 +0300

    I fixed the consecutive calculation problem and I added the roundnumber function for better display

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 let currentNum = '';
 let previousNum = '';
+let result = '';
 let operator = '';
 
-let currentDisplay = document.querySelector(".currentNumber"); 
-let previousDisplay = document.querySelector(".previousNumber"); 
+let currentDisplay = document.querySelector(".currentNumber");
+let previousDisplay = document.querySelector(".previousNumber");
 let equal = document.querySelector(".equal");
-equal.addEventListener("click", calcul);
-let numcurrentNumbers = document.querySelectorAll(".numbers"); 
+equal.addEventListener("click", () => {
+    if (previousNum !== '' && currentNum !== "") {
+        calcul();
+        Dispayresult();
+    }
+});
+let numcurrentNumbers = document.querySelectorAll(".numbers");
 let signs = document.querySelectorAll(".signs");
-let clear = document.querySelector(".C"); 
+let clear = document.querySelector(".C");
 clear.addEventListener("click", () => {
     currentNum = '';
-    currentDisplay.textContent = currentNum;
-})
+    previousNum = '';
+    result = '';
+    operator = '';
+    currentDisplay.textContent = '';
+    previousDisplay.textContent = '';
+});
 let deleteButton = document.querySelector(".Delete");
 deleteButton.addEventListener("click", deleteLastDigit);
 
 function deleteLastDigit() {
-    currentNum = currentNum.slice(0, -1); 
+    currentNum = currentNum.slice(0, -1);
     currentDisplay.textContent = currentNum;
 }
+
 let decimalButton = document.querySelector(".decimal");
 decimalButton.addEventListener("click", addDecimal);
 
 function addDecimal() {
-    if (!currentNum.includes('.')) { 
+    if (!currentNum.includes('.')) {
         currentNum += '.';
         currentDisplay.textContent = currentNum;
     }
 }
+
 numcurrentNumbers.forEach(currentNumBtn => {
     currentNumBtn.addEventListener("click", (e) => {
-        handleNumcurrentNumber(e.target.textContent); 
+        handleNumcurrentNumber(e.target.textContent);
     });
 });
 
@@ -39,53 +51,63 @@ function handleNumcurrentNumber(numcurrentNumber) {
     if (currentNum.length <= 9) {
         currentNum += numcurrentNumber;
         currentDisplay.textContent = currentNum;
+        
     }
 }
 
 signs.forEach(sign => {
     sign.addEventListener("click", (e) => {
         handleOperator(e.target.textContent);
+        
     });
 });
 
 function handleOperator(op) {
-    if (operator.length <= 1) {
-        operator = op;
-        previousNum = currentNum;
-        previousDisplay.textContent = previousNum + " " + operator;
-        currentNum = '';
-        currentDisplay.textContent = '';
+    if (previousNum !== '' && currentNum !== "") {
+        calcul();
     }
+    if (result!== "") {
+        previousNum = result;
+        result = "";
+    } else {
+        previousNum = currentNum;
+    }
+    operator = op;
+
+    previousDisplay.textContent = previousNum + " " + operator;
+    currentNum = '';
+    currentDisplay.textContent = '';
+    result=previousNum+currentNum;
 }
 
 function calcul() {
-    previousNum = Number(previousNum);
+    previousNum = Number(result !== '' ? result : previousNum);
     currentNum = Number(currentNum);
+
     if (operator === "+") {
-        previousNum += currentNum;
+        result = previousNum + currentNum;
     } else if (operator === "-") {
-        previousNum -= currentNum;
+        result = previousNum - currentNum;
     } else if (operator === "*") {
-        previousNum *= currentNum;
+        result = previousNum * currentNum;
     } else if (operator === "/") {
-        if (currentNum === 0) { 
-            previousNum = "Error: Cannot divide by zero";
-            previousDisplay.textContent = "";
-            currentDisplay.textContent = previousNum;
-            currentNum = '';
-            operator = '';
+        if (currentNum === 0) {
+            result = "Error: Cannot divide by zero";
+            
             return;
         }
-        previousNum /= currentNum;
+        result = previousNum / currentNum;
     }
-    previousNum = previousNum.toString();
-    Dispayresult();
-    
+    result
+    result = roundNumber( result.toString());
+
     currentNum = '';
     operator = '';
 }
-
+function roundNumber(num) {
+    return Math.round(num * 100000) / 100000;
+  }
 function Dispayresult() {
     previousDisplay.textContent = "";
-    currentDisplay.textContent = previousNum;
+    currentDisplay.textContent = result;
 }
